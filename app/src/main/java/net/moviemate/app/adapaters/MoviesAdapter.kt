051466f622c5532324a6b9b8d86cdf6952adb49a
moriya2001 <moriyang19@gmail.com>
2025-03-19 18:29:00 +0200

    display all the movies posts on main screen and also delete post functionality added

---
+++ app/src/main/java/net/moviemate/app/adapaters/MoviesAdapter.kt
@@ -0,0 +1,69 @@
+package net.moviemate.app.adapaters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import net.moviemate.app.R
+import net.moviemate.app.databinding.ItemMovieDesignBinding
+import net.moviemate.app.m.Movie
+import net.moviemate.app.utils.UserSession
+
+class MoviesAdapter(private val movies: List<Movie>, private val isActionEnable:Boolean = false) : RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int, movie: Movie)
+        fun onItemDelete(position: Int,movie: Movie)
+    }
+
+    private var mListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.mListener = listener
+    }
+
+    inner class MovieViewHolder(private val binding: ItemMovieDesignBinding,private val mListener: OnItemClickListener) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(movie: Movie) {
+            binding.movieTitle.text = movie.title
+            binding.movieReview.text = movie.review
+            binding.username.text = "Posted by ${movie.username}"
+
+           if (movie.image.isNotEmpty()){
+               Picasso.get()
+                   .load(movie.image)
+                   .placeholder(R.drawable.placeholder)
+                   .into(binding.movieImage)
+           }
+
+            if (movie.userImage.isNotEmpty()){
+                Picasso.get()
+                    .load(movie.userImage)
+                    .placeholder(R.drawable.placeholder)
+                    .into(binding.userImage)
+            }
+
+            if (UserSession.getUser()?.userId == movie.userId){
+                binding.postActionWrapper.visibility = View.VISIBLE
+            }
+            else{
+                binding.postActionWrapper.visibility = View.GONE
+            }
+
+            binding.postDeleteView.setOnClickListener {
+                mListener.onItemDelete(layoutPosition,movie)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding = ItemMovieDesignBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieViewHolder(binding,mListener!!)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movies[position])
+    }
+
+    override fun getItemCount() = movies.size
+}
--- app/src/main/java/net/moviemate/app/v/fragments/MoviesFragment.kt
+++ app/src/main/java/net/moviemate/app/v/fragments/MoviesFragment.kt
@@ -5,13 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import net.moviemate.app.R
+import net.moviemate.app.adapaters.MoviesAdapter
 import net.moviemate.app.databinding.FragmentMoviesBinding
+import net.moviemate.app.m.Movie
+import net.moviemate.app.mvf.MoviesViewModelFactory
+import net.moviemate.app.room.MovieDatabase
+import net.moviemate.app.utils.WrapContentLinearLayoutManager
+import net.moviemate.app.vm.MoviesViewModel
 
 
-class MoviesFragment : Fragment() {
+class MoviesFragment : Fragment(),MoviesAdapter.OnItemClickListener {
     private lateinit var binding:FragmentMoviesBinding
+    private lateinit var moviesAdapter: MoviesAdapter
+    private lateinit var viewModel: MoviesViewModel
+    private var moviesList = mutableListOf<Movie>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val movieDao = MovieDatabase.getDatabase(requireContext()).movieDao()
+        viewModel = ViewModelProvider(this, MoviesViewModelFactory(movieDao))[MoviesViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +44,61 @@ class MoviesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.progressBar.visibility = View.VISIBLE
+        binding.moviesRecyclerview.layoutManager = WrapContentLinearLayoutManager(requireActivity(),RecyclerView.VERTICAL,false)
+        binding.moviesRecyclerview.hasFixedSize()
+        moviesAdapter = MoviesAdapter(moviesList)
+        binding.moviesRecyclerview.adapter = moviesAdapter
+        moviesAdapter.setOnItemClickListener(this)
+
+        viewModel.moviesLiveData.observe(requireActivity()){movies ->
+            binding.progressBar.visibility = View.GONE
+           if (movies.isNotEmpty()){
+               moviesList.clear()
+               moviesList.addAll(movies)
+               moviesAdapter.notifyItemChanged(0,moviesList.size)
+               binding.moviesRecyclerview.visibility = View.VISIBLE
+               binding.emptyListMessageView.visibility = View.GONE
+           }
+            else{
+               binding.moviesRecyclerview.visibility = View.GONE
+               binding.emptyListMessageView.visibility = View.VISIBLE
+           }
+        }
+
         binding.addFabBtn.setOnClickListener {
             findNavController().navigate(R.id.action_moviesFragment_to_addMovieFragment)
         }
+
+
+    }
+
+    override fun onItemClick(position: Int, movie: Movie) {
+
+    }
+
+    override fun onItemDelete(position: Int, movie: Movie) {
+        val builder = MaterialAlertDialogBuilder(requireActivity())
+        builder.setTitle("Delete")
+        builder.setMessage("Are you sure you want to delete movie?")
+        builder.setNegativeButton("Cancel"){dialog,which->
+            dialog.dismiss()
+        }
+        builder.setPositiveButton("Delete"){dialog,which->
+            dialog.dismiss()
+            viewModel.deleteMovie(movie)
+            moviesList.removeAt(position)
+            moviesAdapter.notifyItemChanged(position)
+
+            if (moviesList.isEmpty()){
+                binding.moviesRecyclerview.visibility = View.GONE
+                binding.emptyListMessageView.visibility = View.VISIBLE
+            }
+
+        }
+
+        val alert = builder.create()
+        alert.show()
     }
 
 }
