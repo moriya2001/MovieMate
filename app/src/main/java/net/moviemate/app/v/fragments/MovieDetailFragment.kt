5e4ef91dc19c1dcf451f4942a746ca7e39db3ac2
moriya2001 <moriyang19@gmail.com>
2025-04-08 17:07:49 +0300

    added movie link to MovieDetailFragment

--- app/src/main/java/net/moviemate/app/v/fragments/MovieDetailFragment.kt
+++ app/src/main/java/net/moviemate/app/v/fragments/MovieDetailFragment.kt
@@ -1,5 +1,7 @@
 package net.moviemate.app.v.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,17 +13,15 @@ import net.moviemate.app.R
 import net.moviemate.app.databinding.FragmentMovieDetailBinding
 import net.moviemate.app.m.Movie
 
-
 class MovieDetailFragment : Fragment() {
 
-    private lateinit var binding:FragmentMovieDetailBinding
-    private var movie:Movie?=null
-
+    private lateinit var binding: FragmentMovieDetailBinding
+    private var movie: Movie? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
         val args: MovieDetailFragmentArgs by navArgs()
         movie = args.movie
@@ -31,19 +31,44 @@ class MovieDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.movieTitle.text = movie!!.title
-        binding.movieReview.text = movie!!.review
-        binding.username.text = "Posted by ${movie!!.username}"
+        // Title and review
+        binding.movieTitle.text = movie?.title
+        binding.movieReview.text = movie?.review
 
-        if (movie!!.image.isNotEmpty()){
+        // Show "Posted by username"
+        binding.username.text = "Posted by ${movie?.username}"
+
+        // Load the user profile image
+        if (!movie?.userImage.isNullOrEmpty()) {
             Picasso.get()
-                .load(movie!!.image)
+                .load(movie?.userImage)
                 .placeholder(R.drawable.loader)
-                .resize(600,400)
+                .error(R.drawable.placeholder)
+                .into(binding.userImage)
+        }
+
+        // Load the movie poster
+        if (!movie?.image.isNullOrEmpty()) {
+            Picasso.get()
+                .load(movie?.image)
+                .placeholder(R.drawable.loader)
+                .resize(600, 400)
                 .centerCrop()
                 .error(R.drawable.placeholder)
                 .into(binding.movieImage)
         }
-    }
 
+        // Show the movie link
+        binding.movieLink.text = movie?.link
+
+        // If you want the link to open in a browser when clicked, set an onClickListener:
+        binding.movieLink.setOnClickListener {
+            movie?.link?.let { link ->
+                if (link.isNotEmpty()) {
+                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+                    startActivity(browserIntent)
+                }
+            }
+        }
+    }
 }
